77c80576e32056fa66e4db1055a9727c9e32dc6f
@LaibaAshfaq2 <laibaashfaqahmed232@gmail.com>
2026-01-30 16:52:04 +0500

    fixing lock file error

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,15 @@
 // frontend/next.config.js
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig: import('next').NextConfig = {
   reactStrictMode: true,
   // Add this if you have custom PostCSS
   experimental: {
     optimizePackageImports: ['tailwindcss'],
+  },
+  // Add turbopack configuration
+  turbopack: {
+    root: path.join(__dirname, '../../'),
   },
 };
 
