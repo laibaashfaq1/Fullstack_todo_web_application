5d385718bb134e86fef49d6106aa6b3de2fb1067
@LaibaAshfaq2 <165155950+laibaashfaq1@users.noreply.github.com>
2026-03-14 12:23:52 +0500

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,16 @@
-// frontend/next.config.js
+import path from "path";
+
 /** @type {import('next').NextConfig} */
-const path = require('path');
-const nextConfig: import('next').NextConfig = {
-  reactStrictMode: true,
+const nextConfig = {
   experimental: {
     optimizePackageImports: ['tailwindcss'],
-    outputFileTracingRoot: path.join(__dirname, '../../'), // Explicitly set output file tracing root
   },
+
+  outputFileTracingRoot: path.join(__dirname, '../../'),
+
   turbopack: {
     root: path.join(__dirname, '../../'),
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
